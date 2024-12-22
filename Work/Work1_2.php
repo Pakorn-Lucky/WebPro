c2393a2da64c021a9e5657a85a7155ec9277730c
Pakorn-Lucky <popo12010@hotmail.com>
2024-12-22 23:19:38 +0700

    Update Work1_2.php

--- Work/Work1_2.php
+++ Work/Work1_2.php
@@ -1,10 +1,9 @@
 <?php
-if (isset($_GET['hw']) && isset($_GET['midterm']) && isset($_GET['final'])) {
+if (isset($_GET['submit'])) {
     $hw = $_GET['hw'];
     $midterm = $_GET['midterm'];
     $final = $_GET['final'];
-
-    // Validate scores to ensure they are within the allowed range
+    
     if ($hw > 30)
         $hw = 30;
     if ($midterm > 35)
@@ -12,73 +11,69 @@ if (isset($_GET['hw']) && isset($_GET['midterm']) && isset($_GET['final'])) {
     if ($final > 35)
         $final = 35;
 
-    // Display scores
-    echo "<p><b>ข้อมูลคะแนน</b><br />";
-    echo "Homework: <i>$hw</i> <br />";
-    echo "Midterm: <i>$midterm</i> <br />";
-    echo "Final: <i>$final</i> <br />";
+    echo "<b>ข้อมูลคะแนน </b><br />";
+    echo "Homework : <i> $hw </i> <br/>";
+    echo "Midterm : <i> $midterm </i> <br/>";
+    echo "Final : <i> $final </i> <br/>";
 
-    // Calculate total score and grade
     $total = $hw + $midterm + $final;
-    echo "Total Score: $total <br />";
+    echo "Total Score : $total <br>";
 
-    if ($total >= 80) {
-        echo "Result Grade: A<br />";
-    } elseif ($total >= 75) {
-        echo "Result Grade: B+<br />";
-    } elseif ($total >= 70) {
-        echo "Result Grade: B<br />";
-    } elseif ($total >= 65) {
-        echo "Result Grade: C+<br />";
-    } elseif ($total >= 60) {
-        echo "Result Grade: C<br />";
-    } elseif ($total >= 55) {
-        echo "Result Grade: D+<br />";
-    } elseif ($total >= 50) {
-        echo "Result Grade: D<br />";
-    } else {
-        echo "Result Grade: F<br />";
-    }
+    if ($total >= 80)
+        echo "Result Grade : A<br>";
+    elseif ($total >= 75)
+        echo "Result Grade : B+<br>";
+    elseif ($total >= 70)
+        echo "Result Grade : B<br>";
+    elseif ($total >= 65)
+        echo "Result Grade : C+<br>";
+    elseif ($total >= 60)
+        echo "Result Grade : C<br>";
+    elseif ($total >= 55)
+        echo "Result Grade : D+<br>";
+    elseif ($total >= 50)
+        echo "Result Grade : D<br>";
+    else
+        echo "Result Grade : F<br>";
 
-    echo "<br /><a href=''> <big>Back</big></a></p>";
+    echo "<a href='Work1_2.php'> <big>Back </big></a>";
+} else { ?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>การสร้างฟอร์มในการรับค่าเพื่อคำนวณเกรด</title>
+    </head>
+
+    <body>
+        <form method="get" action="Work1_2.php">
+            <table border="1" align="center" width="500">
+                <tr>
+                    <td colspan="2" align="center">
+                        <big>ทดสอบการใช้ Arithmatic Operator </big>
+                    </td>
+                <tr>
+                <tr>
+                    <td>Enter Home work : </td> 
+                    <td><input type="number" name="hw" size="15" min="0" max="30" value="" /> </td>
+                </tr>
+                <td>Enter Midterm : </td>
+                <td><input type="number" name="midterm" size="15" min="0" max="35" value="" /></td>
+                </tr>
+                <td>Enter final : </td>
+                <td><input type="number" name="final" size="15"  min="0" max="35" value="" /></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                    <input type="submit" name="submit" value=" OK " />
+                    <input type="reset" name="reset" value=" Clear " />
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </body>
+
+    </html>
+    <?php
 }
-{?>
-<html>
-
-<head>
-    <title>การสร้างฟอร์มในการรับค่าเพื่อคำนวณเกรด</title>
-</head>
-
-<body>
-    <form method="get" action="lab6-11cal.php">
-        <table border="1" align="center" width="500">
-            <tr>
-                <td colspan="2" align="center">
-                    <big>ทดสอบการใช้ Arithmatic Operator </big>
-                </td>
-            <tr>
-            <tr>
-                <td>Enter Home work : </td>
-                <td><input type="number" name="hw" min="0" max="30" size="15" value="" /> </td>
-            </tr>
-            <td>Enter Midterm : </td>
-            <td><input type="number" name="midterm" size="15" min="0" max="35" value="" /></td>
-            </tr>
-            </tr>
-            <td>Enter final : </td>
-            <td><input type="number" name="final" min="0" max="35"size="15" value="" /></td>
-            </tr>
-            <tr>
-                <td colspan="2" align="center">
-                    <input type="submit" value=" OK " />
-                    <input type="reset" value=" Clear " />
-                </td>
-            </tr>
-        </table>
-    </form>
-</body>
-
-</html>
-
-<?php
-}
+?>
